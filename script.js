f77c715adc1a34ac96d4131582e877de9afe07be
sy coding <sybossgamer036@gmail.com>
2023-08-04 16:16:31 +0100

    gh

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var mb = document.getElementById('menuBtn');
-        var sv = document.getElementById('sidenav');
+var sv = document.getElementById('sidenav');
+    sv.style.right = "-250px";
+    mb.onclick =function(){
+        if(sv.style.right == "-250px"){
+            sv.style.right = "0";
+        }else{
         sv.style.right = "-250px";
-        mb.onclick =function(){
-            if(sv.style.right == "-250px"){
-                sv.style.right = "0";
-            }else{
-                sv.style.right = "-250px";
-            }
         }
+    }
